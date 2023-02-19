865b7ca2330bd18d6ef5a6a13ce072e50f206705
Matteo Rebecchi Mari <dev.matteo.rebecchimari@gmail.com>
2023-02-19 15:03:13 +0100

    Added ClockHands for seconds and minutes. Added tick numbers.

--- ComposeClock/app/src/main/java/com/matteo/rebecchimari/composeclock/MainActivity.kt
+++ ComposeClock/app/src/main/java/com/matteo/rebecchimari/composeclock/MainActivity.kt
@@ -7,8 +7,10 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -16,9 +18,18 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.font.FontWeight.Companion.Bold
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.max
+import androidx.compose.ui.unit.sp
 import com.matteo.rebecchimari.composeclock.ui.theme.ComposeClockTheme
+import java.util.Collections.rotate
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,32 +50,45 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ClockStateful() {
-    Box(modifier = Modifier.padding(10.dp),
-        contentAlignment = Alignment.Center
-        ) {
-        ClockDialStateless()
-        ClockHandHour()
+
+    ClockDialStateless()
+    {
+        // Seconds
+        ClockHand( 0.9f, 2, Color.Green)
+        // Minutes
+        ClockHand( 0.7f, 5, Color.Red)
+        // Hours
+        ClockHand( 0.4f, 10, Color.Black)
     }
+
 }
 
 @Composable
-fun ClockDialStateless(){
+fun ClockDialStateless(
+    dialColor: Color = Color.White,
+    ticksColor: Color = Color.Black,
+    content: @Composable() () -> Unit
+){
     Box(
         modifier = Modifier
             .fillMaxWidth()
             .aspectRatio(ratio = 1f)
             .clip(CircleShape)
-            .background(Color.Green)
+            .background(dialColor)
             .border(
                 width = 2.dp,
-                color = Color.Black,
-                shape = CircleShape),
+                color = ticksColor,
+                shape = CircleShape
+            ),
         contentAlignment = Alignment.Center
-    ) {
+    )
+    {
+
         // Ticks
         for (i in 0..11){
-            Tick(i)
+            Tick(i,ticksColor)
         }
+
         // Dial (cover ticks)
         Box(
             Modifier
@@ -72,30 +96,98 @@ fun ClockDialStateless(){
                 .padding(10.dp)
                 .aspectRatio(ratio = 1f)
                 .clip(CircleShape)
-                .background(Color.Green)
+                .background(dialColor)
         )
+
+        // Ticks numbers
+        for (i in 12 downTo 1){
+            TickNumber(i-12, 12, "$i")
+        }
+
+
+        // Content
+        content()
+
     }
 }
 
 @Composable
-fun Tick(position: Int){
+fun Tick(position: Int, color: Color = Color.Black){
     Box(modifier = Modifier
         .fillMaxHeight()
         .width(2.dp)
         .rotate(360f / 12 * position)
         .clip(RectangleShape)
-        .background(Color.Black)
+        .background(color)
     )
 }
 
 @Composable
-fun ClockHandHour(modifier: Modifier = Modifier){
-    Box(modifier = modifier
-        .fillMaxHeight(0.3f)
-        .width(10.dp)
-        .clip(RectangleShape)
-        .background(Color.Black)
-    )
+fun TickNumber(
+    position: Int,
+    maxPosition: Int,
+    text: String,
+    color: Color = Color.Black
+){
+    val rotation = 360f / maxPosition * position
+
+    fun GetFW(): FontWeight?
+    {
+        var fw: FontWeight? = null
+        if(position % 3 == 0){
+            fw = FontWeight.Bold
+        }
+        return fw
+    }
+
+    Column ( modifier = Modifier
+        .fillMaxHeight()
+        .rotate(rotation),
+        verticalArrangement = Arrangement.Top) {
+
+        Text(
+            modifier = Modifier
+                .rotate(-rotation)
+                .padding(10.dp),
+            text = text,
+            color = color,
+            fontSize = 27.sp,
+            textAlign = TextAlign.Center,
+            fontWeight = GetFW()
+        )
+
+    }
+
+}
+
+@Composable
+fun ClockHand(
+    heightPercentage: Float = 0.3f,
+    width: Int = 10,
+    color: Color = Color.Black,
+    modifier: Modifier = Modifier
+){
+    val shape = RoundedCornerShape((width*0.5).dp)
+    Column(
+        modifier = modifier
+            .fillMaxHeight(heightPercentage)
+            .width((width).dp)
+            .offset(0.dp, 5.dp)) {
+        val totalHeight = IntrinsicSize.Max;
+        Box(modifier = modifier
+            .weight(0.5f) // Weight to split the total height
+            .fillMaxWidth()
+            .clip(shape)
+            .background(color)
+        )
+        Box(modifier = modifier
+            .weight(0.5f) // Weight to split the total height
+            .fillMaxWidth()
+            .clip(shape)
+            .background(Color.Transparent)
+        )
+    }
+
 }
 
 @Preview(showBackground = true, device = "id:pixel_5")
